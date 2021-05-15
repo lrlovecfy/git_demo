bd7e09e7c5ea09acbd3446402d5f4090712525a5
Layne <lrsuoxin@hotmail.com>
2021-05-15 22:48:49 +0800

    hot-fix commit

--- src/main/java/cn/itcast/GitTest.java
+++ src/main/java/cn/itcast/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Lr");
         System.out.println("Hello Cfy");
+        System.out.println("hot-fix");
     }
 }
